61e7b41bbfcc9fc64d392bc2033eebc73509b40f
Lucas-Mierisch <123103146+Lucas-Mierisch@users.noreply.github.com>
2023-04-10 21:00:19 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,36 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy <= 8) {
+        mySprite.vy = -200
+    }
+})
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+controller.moveSprite(mySprite, 100, 0)
+forever(function () {
+    if (mySprite.isHittingTile(CollisionDirection.Top)) {
+        mySprite.vy = 0
+    } else {
+        if (mySprite.vy < 200) {
+            mySprite.vy += 8
+        }
+    }
+})
